7207bc3c774ab71d52b9a52514d09774e45fa343
saulgn2007 <saulgonzaleznavarro2007@gmail.com>
2025-02-20 09:59:01 +0100

    Terminados

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,2 +1,15 @@
 # Escribe una función recursiva que reciba una cadena y devuelva True si es un palíndromo y False 
 # en caso contrario. Una palabra es palíndroma si se lee igual de un lado que de otro.
+
+def es_palindromo(cadena):
+
+    if len(cadena) <= 1:
+        return True
+
+    if cadena[0] != cadena[-1]:
+        return False
+
+    return es_palindromo(cadena[1:-1])
+
+print(es_palindromo('reconocer'))
+print(es_palindromo('arpa'))
--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,1 +1,12 @@
 # Implementa el algoritmo de Bubble Sort para ordenar una lista de números de menor a mayor.
+
+def bubble_sort(lista):
+    n = len(lista)
+    for i in range(n):
+        for j in range(n):
+            if lista[i] < lista[j]:
+                lista[i], lista[j] = lista[j], lista[i]
+    return lista
+             
+lista_ordenada = bubble_sort([8, 6, 1, 4, 9, 2, 5, 7, 3])
+print(lista_ordenada)
