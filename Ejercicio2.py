38caaeb4e5a047a5d0884cee664d2d61f2633e41
saulgn2007 <saulgonzaleznavarro2007@gmail.com>
2025-02-20 09:29:10 +0100

    Ejercicio 2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,2 +1,14 @@
 # Crea una función que genere una contraseña aleatoria de longitud especificada, incluyendo 
 # letras, números y caracteres especiales.
+
+import random
+
+def Contraseña(longitud):
+    letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    numeros = '0123456789'
+    caracteres_especiales = '!@#$%^&*()-_=+[]{}|;:,.<>?/~`'
+    caracteres = letras + numeros + caracteres_especiales
+    contraseña = ''.join(random.choice(caracteres) for i in range(longitud))
+    return contraseña
+
+print(Contraseña(12))
